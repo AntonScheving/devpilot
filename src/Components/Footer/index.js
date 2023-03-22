d9b2144b117ed156e6ed74bae4f5b839516504df
Vinita Yadav <yadav.vinita@gmail.com>
2023-03-22 17:44:27 +0000

    added basic components n pages

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style.css"
+
+const Footer = () => {
+  return (
+    <div>This is a Footer</div>
+  )
+}
+
+export default Footer;
---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Contact from '../../pages/Contact';
+import About from '../../pages/About';
+
+const Navbar = () => {
+  return (
+    <div>
+      <About />
+      <Contact />
+    </div>
+  )
+}
+
+export default Navbar;

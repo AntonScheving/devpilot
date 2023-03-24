9c7f781f6306a56b4cd6328d3a8526db330472be
maudmain <117314424+maudmain@users.noreply.github.com>
2023-03-24 16:18:10 +0000

    bg added for FeatureGrid

--- src/Components/FeaturedGrid/index.js
+++ src/Components/FeaturedGrid/index.js
@@ -1,26 +1,33 @@
 import React from 'react'
 import FeaturedCompanies from '../FeaturedCompanies';
-import { Grid } from '@mui/material';
+import { Grid, Box } from '@mui/material';
 import FeaturedListings from '../FeaturedListings';
+import { styled } from "@mui/material/styles";
+
+const Root = styled(Box)(({ theme }) => ({
+  backgroundColor: `${ theme.palette.background.default }`
+}))
+
 
 const FeaturedGrid = () => {
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{
-        border: "1px solid blue",
-        m: 4,
-      }}
-    >
-      <Grid item xs={12} sm={6}>
-        <FeaturedCompanies />
+    <Root>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          border: "1px solid blue",
+          mx: 4,
+        }}
+      >
+        <Grid item xs={12} sm={6}>
+          <FeaturedCompanies />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FeaturedListings />
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <FeaturedListings />
-      </Grid>
-
-    </Grid>
+    </Root>
   );
 };
 

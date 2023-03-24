24a84912128afbd42e8583f4f81595eb48268149
maudmain <117314424+maudmain@users.noreply.github.com>
2023-03-24 16:17:48 +0000

    hero div added

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from 'react';
+import { Box, Button, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const Root = styled(Box)(({ theme }) => ({
+  backgroundImage: `linear-gradient(0deg, ${theme.palette.background.default} 22%, ${theme.palette.primary.main} 90%)`, // Set the background gradient colors
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+  /* padding: "0 20px",*/
+}));
+
+const Image = styled("img")({
+  maxWidth: "100%",
+  height: "auto",
+  marginRight: "50px",
+});
+
+const ButtonWrapper = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  marginTop: "50px",
+});
+
+const CustomButton = styled(Button)(({ theme }) => ({
+  marginRight: "10px",
+  color: theme.palette.primary.contrastText,
+  backgroundColor: theme.palette.info.main,
+  "&:hover": {
+    backgroundColor: theme.palette.secondary.dark,
+  },
+}));
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
+    // root component: the outermost component that renders a host component
+    <Root>
+      <Image src="" alt="Image by upklyak on Freepik" />
+      {/* <Typography variant="caption" sx={{ color: "common.white" }}>
+        Image by{" "}
+        <a
+          href="https://www.freepik.com/free-vector/job-search-banner-we-are-hiring-recruitment-ads_26474572.htm#page=2&position=14&from_view=author"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          upklyak
+        </a>{" "}
+        on Freepik
+      </Typography> */}
+      <div>
+        <Typography
+          variant="h2"
+          component="h1"
+          gutterBottom
+          sx={{ color: "common.white" }}
+        >
+          Find your dream job
+        </Typography>
+        <Typography variant="h5" gutterBottom sx={{ color: "common.white" }}>
+          The best carreer opportunities happen here
+        </Typography>
+        
+        <ButtonWrapper>
+          <CustomButton variant="contained" color="info">
+            JOBS
+          </CustomButton>
+          <CustomButton variant="contained">EMPLOYERS</CustomButton>
+        </ButtonWrapper>
+      </div>
+    </Root>
+  );
 }
+ 
 
 export default Hero;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import FeaturedGrid from '../Components/FeaturedGrid';
+import Hero from '../Components/Hero';
 
 const Home = () => {
   return (
     <div>
     
           <Link to="jobs"> Jobs</Link>
-          <Link to="companies">Companies</Link>
+      <Link to="companies">Companies</Link>
+      <Hero/>
           <FeaturedGrid/>
     </div>
   );

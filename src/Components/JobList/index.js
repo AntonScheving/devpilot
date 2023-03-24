0ec13459b5ec6e67a5930f58486e96b20161647b
Vinita Yadav <yadav.vinita@gmail.com>
2023-03-24 16:07:26 +0000

    added joblist comp to jobs page and a search filter comp

---
+++ src/Components/JobList/index.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+
+const JobList = () => {
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=6c3aabdd&app_key=965a2d73c4df5e1c3f1e7c86b9b50096&results_per_page=45&what=Front%20end%20developer&what_or=Front%20end%20web%20developer&what_exclude=senior")
+        .then(response => response.json())
+        .then(data => setJobs(data.results));
+    }, []);
+
+  return (
+    <div>
+        {jobs.map((item) => (
+            <div key={item.id}>
+                <div>
+                    <a href={item.redirect_url}>
+                    <h3>{item.title}</h3></a>
+                    <p>{item.company.display_name}</p>
+                </div>
+
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default JobList
--- src/pages/Companies.js
+++ src/pages/Companies.js
@@ -5,6 +5,7 @@ const CompaniesPage = () => {
   return (
     <>
       <CompaniesHiring />
+  
     </>
   );
 };

6dede6d5005f39229359829ab6c2931743141696
maudmain <117314424+maudmain@users.noreply.github.com>
2023-03-25 16:18:04 +0000

    MUI Appbar with drawer added inc dependencies

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,8 +1,11 @@
-import { Typography, Button } from "@mui/material";
+import { Typography, Button, IconButton, Drawer, MenuItem } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import React from "react";
-import { Link as RouterLink } from "react-router-dom";
+import {Link,  Link as RouterLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const headersData = [
   {
@@ -23,42 +26,129 @@ const headersData = [
   },
 ];
 
+const menuButton = styled("button")({
+  fontWeight: 700,
+  fontSize: "18px",
+  marginLeft: "38px",
+});
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
 export default function Header() {
+  const [state, setState] = useState({
+    mobileView: false,
+    drawerOpen: false,
+  });
+
+  const { mobileView, drawerOpen } = state;
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+
+    setResponsiveness();
+
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    };
+  }, []);
+
   const displayDesktop = () => {
     return (
-      <Toolbar>
+      <StyledToolbar className="toolbar">
         {devPilotLogo}
-        {getMenuButtons()}
-      </Toolbar>
+        <div>{getMenuButtons()}</div>
+      </StyledToolbar>
     );
   };
 
-  const devPilotLogo = (
-    <Typography variant="h6" component="h1">
-      DevPilot
-    </Typography>
-  );
+    const displayMobile = () => {
+        const handleDrawerOpen = () =>
+            setState((prevState) => ({ ...prevState, drawerOpen: true }));
+        const handleDrawerClose = () =>
+            setState((prevState) => ({ ...prevState, drawerOpen: false }));
+        return (
+          <Toolbar>
+            <IconButton
+              {...{
+                edge: "start",
+                color: "inherit",
+                "aria-label": "menu",
+                "aria-haspopup": "true",
+                onClick: handleDrawerOpen,
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
 
-  const getMenuButtons = () => {
-    return headersData.map(({ label, href }) => {
-      return (
-        <Button
-          {...{
-            key: label,
-            color: "inherit",
-            to: href,
-            component: RouterLink,
-          }}
-        >
-          {label}
-        </Button>
-      );
-    });
+            <Drawer
+              {...{
+                anchor: "left",
+                open: drawerOpen,
+                onClose: handleDrawerClose,
+              }}
+            >
+              <div>{getDrawerChoices()}</div>
+            </Drawer>
+            <div>{devPilotLogo}</div>
+          </Toolbar>
+        );
+    }
+
+    const getDrawerChoices = () => {
+      return headersData.map(({ label, href }) => {
+        return (
+          <Link
+            {...{
+              component: RouterLink,
+              to: href,
+              color: "inherit",
+              style: { textDecoration: "none" },
+              key: label,
+            }}
+          >
+            <MenuItem>{label}</MenuItem>
+          </Link>
+        );
+      });
+    };
+
+    const devPilotLogo = (
+      <Typography variant="h3" component="h1">
+        DevPilot
+      </Typography>
+    );
+
+    const getMenuButtons = () => {
+      return headersData.map(({ label, href }) => {
+        return (
+          <Button
+            {...{
+              key: label,
+              color: "inherit",
+              to: href,
+              component: RouterLink,
+              className: menuButton,
+            }}
+          >
+            {label}
+          </Button>
+        );
+      });
+    };
+
+    return (
+      <header>
+        <AppBar>{mobileView ? displayMobile() : displayDesktop()}</AppBar>
+      </header>
+    );
   };
 
-  return (
-    <header>
-      <AppBar>{displayDesktop()}</AppBar>
-    </header>
-  );
-}

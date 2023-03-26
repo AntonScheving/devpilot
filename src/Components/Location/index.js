e7380f162ae82a0ec2a37d1cc6b3fd2b0c3ad48c
Vinita Yadav <yadav.vinita@gmail.com>
2023-03-26 19:05:28 +0100

    wrapped location and joblist comp in another comp

--- src/Components/Location/index.js
+++ src/Components/Location/index.js
@@ -1,31 +1,66 @@
 import React, { useState } from 'react'
+import { Box, Typography } from "@mui/material";
+import TextField from '@mui/material/TextField';
+
+import {  useTheme } from "@mui/material/styles";
 import JobList from '../JobList';
 
 const Location = () => {
+  const theme = useTheme();
     const [location, setLocation] = useState('');
     const [apiData, setApiData] = useState([]);
 
     const handleSearch = (e) => {
-    setLocation(e.target.value);
+      console.log('handleSearch called');
+    const userInput = e.target.value;
+    setLocation(userInput);
   }
   
 
 const fetchData = async () => {
-
+ console.log('fetchData called');
   const response = await fetch(`https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=6c3aabdd&app_key=0f15a296c6a265d2e7c0fb2fbe7cb467&results_per_page=10&what=junior%20developer&where=${location}`);
   const data = await response.json();
   setApiData(data.results);
 }
+
+// const Root = styled(Box)(({ theme }) => ({
+//     backgroundColor: `${theme.palette.primary.main}`,
+//     margin: 30,
+//     borderRadius: 25,
+//     height: "100%",
+//   }));
+
     return (
     <>
-    <div>
-     {/* // conditional render which happens if the apiData array is empty n JobList comp renders  */}
-      {/* {apiData.length === 0 && <JobList />} */}
-        <input type="text"
-        placeholder='Location'
-        value={location}
-        onChange={handleSearch}/>
-        <button onClick={fetchData}>Search</button>
+    <Box
+    sx={{
+          margin: 4,
+          fontFamily: `{'Lato', sans-serif;}`,
+        }}
+      >
+      <Typography variant="h4" 
+      mb={1}
+      color={theme.palette.text.tertiary}>
+          {""}
+          Filter/Search
+        </Typography> 
+          <TextField
+          id="outlined-search"
+          label="Jobs by Location"
+          type="search"
+          value={location}
+        onChange={handleSearch}
+        onKeyPress={(event) => {
+          if (event.key === 'Enter') {
+            fetchData();
+          }
+        }}
+        InputProps={{ style: { backgroundColor: '#f0f0f0' } }}
+        /> 
+        
+          {/* // conditional render which happens if the apiData array is empty n JobList comp renders  */}
+          {!apiData.length && <JobList />} 
         {apiData.map((job) => (
             <div key={job.id}>
                 <div>
@@ -37,8 +72,8 @@ const fetchData = async () => {
             
             
         ))}
-        </div>
-    
+        
+      </Box>
     </>
   )
 }
